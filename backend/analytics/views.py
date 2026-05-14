907cec8d90b09080afe863f16343f409abe6eb25
squid-protocol <joe.m.esquibel@gmail.com>
2026-05-14 19:56:19 -0400

    chore: remove unused imports

--- backend/analytics/views.py
+++ backend/analytics/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.db.models import Sum
-from django.utils import timezone
-from datetime import timedelta
 import plotly.graph_objects as go
 import pandas as pd
 
