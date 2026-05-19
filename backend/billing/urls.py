c04aa893c49403771a0237f98012bdf9790711dc
squid-protocol <joe.m.esquibel@gmail.com>
2026-05-19 17:57:36 -0400

    chore: merge conflict resolution

--- backend/billing/urls.py
+++ backend/billing/urls.py
@@ -8,10 +8,6 @@ urlpatterns = [
         name="create_checkout_session",
     ),
     path("success/", views.billing_success, name="billing_success"),
-    # Add this line!
-<<<<<<< HEAD
     path("portal/", views.customer_portal, name="customer_portal"),
-=======
->>>>>>> main
     path("webhook/", views.stripe_webhook, name="stripe_webhook"),
 ]
