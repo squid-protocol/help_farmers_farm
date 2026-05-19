46d306fe0e3fa6c811aa0b8c2cdd9fed26a1ef87
squid-protocol <joe.m.esquibel@gmail.com>
2026-05-19 09:08:11 -0400

    chore: black and flake

--- backend/helpfarmers/urls.py
+++ backend/helpfarmers/urls.py
@@ -9,13 +9,13 @@ urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
     path("accounts/", include("accounts.urls")),
     path("", TemplateView.as_view(template_name="landing.html"), name="home"),
-    
     # --- ADD THESE THREE LINES ---
     path("about/", TemplateView.as_view(template_name="about.html"), name="about"),
     path("faq/", TemplateView.as_view(template_name="faq.html"), name="faq"),
-    path("contact/", TemplateView.as_view(template_name="contact.html"), name="contact"),
+    path(
+        "contact/", TemplateView.as_view(template_name="contact.html"), name="contact"
+    ),
     # -----------------------------
-    
     path("", include("logs.urls")),
     path("farm/", include("farms.urls")),
     path("analytics/", include("analytics.urls")),
