5de14687d107a4534d6d935285ab04e46785fedc
squid-protocol <joe.m.esquibel@gmail.com>
2026-06-11 07:39:51 -0400

    ci: disable SECURE_SSL_REDIRECT for automated testing to resolve 301 errors

--- backend/helpfarmersfarm/settings.py
+++ backend/helpfarmersfarm/settings.py
@@ -4,7 +4,7 @@ import environ
 import sentry_sdk
 import sys
 
-TESTING = "test" in sys.argv
+TESTING = "test" in sys.argv or os.environ.get("GITHUB_ACTIONS") == "true"
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
